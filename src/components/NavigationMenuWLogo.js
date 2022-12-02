fd551f8bfbb3a1354b9d669be31f29115167e501
Manny <justmanneh@gmail.com>
2022-12-02 12:20:42 -0600

    mobile nav menu fully integrated

--- src/components/NavigationMenuWLogo.js
+++ src/components/NavigationMenuWLogo.js
@@ -1,21 +1,43 @@
 import * as React from 'react';
+import {useState} from 'react';
 import '../assets/styles/NavigationMenuWLogo.css';
 import PropTypes from 'prop-types';
 
 const NavigationMenuWLogo = (props) => {
     const {logo, menu} = props;
+    const [openMobileNav, setOpenMobileNav] = useState(false);
+
+    const handleOpenMobileNav = () => {
+        setOpenMobileNav(!openMobileNav);
+    }
+
+    const handleMenuClick = () => {
+        setOpenMobileNav(false);
+    }
+
+    const displayFlex = openMobileNav ? "displayFlex" : "";
 
     return(
         <nav className="navigationMenu">
-            <img src={logo} alt="wgulabs logo" />
+            <img className="logo" src={logo} alt="wgulabs logo" />
             <div className="spacer"></div>
-            <ul>
-                {menu.map((menuItem, index) => {
-                    return (<li key={index}>
-                        <a href={menuItem.link}>{menuItem.text}</a>
-                    </li>);
-                })}
-            </ul>
+
+            <div className="">
+                <div className="icon" onClick={handleOpenMobileNav}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <ul className={displayFlex}>
+                    {menu.map((menuItem, index) => {
+                        return (<li key={index}>
+                            <a onClick={handleMenuClick} href={menuItem.link}>{menuItem.text}</a>
+                        </li>);
+                    })}
+                </ul>
+            </div>
+            
             <div className="spacer"></div>
         </nav>
     );
